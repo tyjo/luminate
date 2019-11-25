20f3e8509d38ee63232bf25a4c1c345728893527
tjoseph <tjoseph@cs.columbia.edu>
2019-11-25 17:24:20 -0500

    new examples

--- run-examples.sh
+++ run-examples.sh
@@ -1,18 +1,28 @@
 #!/usr/bin/env bash
 
+python main.py plot \
+               datasets/bucci2016mdsine/cdiff-counts-pred.csv \
+               -e datasets/bucci2016mdsine/cdiff-events.csv \
+               -i datasets/bucci2016mdsine \
+               -o datasets/bucci2016mdsine
+
 python main.py estimate \
                datasets/bucci2016mdsine/cdiff-counts.csv \
                -e datasets/bucci2016mdsine/cdiff-events.csv \
                -o datasets/bucci2016mdsine
 
+
 python main.py train \
                datasets/bucci2016mdsine/cdiff-counts.csv \
                -e datasets/bucci2016mdsine/cdiff-events.csv \
                -i datasets/bucci2016mdsine \
-               -o datasets/bucci2016mdsine
+               -o datasets/bucci2016mdsine \
+               -b 200
 
 python main.py predict \
-               datasets/bucci2016mdsine/cdiff-counts.csv \
+               datasets/bucci2016mdsine/cdiff-counts-est.csv \
                -e datasets/bucci2016mdsine/cdiff-events.csv \
                -i datasets/bucci2016mdsine \
-               -o datasets/bucci2016mdsine
+               -o datasets/bucci2016mdsine \
+               --one-step
+
